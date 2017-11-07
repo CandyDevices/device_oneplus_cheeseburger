d614db34209fb0bbe43e9be5e9c724fa0a2fe1d4
Dan Cartier <nospamdan1@gmail.com>
2017-11-07 12:47:17 -0500

    op5: Update setup-makefiles script
    Cosmetic only

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -26,9 +26,9 @@ INITIAL_COPYRIGHT_YEAR=2017
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-SLIM_ROOT="$MY_DIR"/../../..
+CANDY_ROOT="$MY_DIR"/../../..
 
-HELPER="$SLIM_ROOT"/vendor/slim/build/tools/extract_utils.sh
+HELPER="$CANDY_ROOT"/vendor/slim/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -36,7 +36,7 @@ fi
 . "$HELPER"
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$SLIM_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$CANDY_ROOT"
 
 # Copyright headers and guards
 write_headers
