a347aee0a8a8abb5aad2ee53a4774f3ce5c43001
Dan Cartier <nospamdan1@gmail.com>
2017-12-16 17:01:44 -0500

    op5 Clean up setup-makefiles

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -28,7 +28,7 @@ if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
 CANDY_ROOT="$MY_DIR"/../../..
 
-HELPER="$CANDY_ROOT"/vendor/slim/build/tools/extract_utils.sh
+HELPER="$CANDY_ROOT"/vendor/candy/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
